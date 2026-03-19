40672744911af8ff7f64018abfc85cdebff9085c
Xuân Đại <daik5xuanvinh@gmail.com>
2026-03-19 15:46:57 +0700

    Delete Tao_co_so_du_lieu_va_bang_quan_ly_sach.sql

--- Tao_co_so_du_lieu_va_bang_quan_ly_sach.sql
+++
@@ -1,9 +0,0 @@
-create schema library;
-
-create table library.books(
-    book_id serial primary key ,
-    title varchar(100) not null ,
-    author varchar(50) not null ,
-    published_year int ,
-    price float
-);
